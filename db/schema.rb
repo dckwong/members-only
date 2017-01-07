61936cbc8f30dfd752c1847a7aaa3c9bbe743bcf
Derek <derek_kwong8@yahoo.com>
2017-01-07 01:49:03 -0800

    Fixed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107082744) do
+ActiveRecord::Schema.define(version: 20170107094547) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
+    t.string   "content"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
